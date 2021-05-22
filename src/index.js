f07204a487f8ef27cf476e2f1c82f4d3555354ac
AniRamadoss <55056278+AniRamadoss@users.noreply.github.com>
2021-05-21 20:56:15 -0400

    some changes

--- src/index.js
+++ src/index.js
@@ -80,7 +80,3 @@ class Timer extends React.Component {
 
 ReactDOM.render(<Timer />, document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
